9ee89bf56d7490b30fccaa09c9dbe635933bed57
NahuelValencia <nahuel.valencia21@gmail.com>
2020-04-12 20:03:43 -0300

    Prepare board and play function added

--- js/models.js
+++ js/models.js
@@ -3,11 +3,27 @@ var TatetiGame = function(player, state) {
     this._state = state;
 }
 
-var Player = function(name, cardSelected) {
+var Player = function(name, pieceSelected) {
     this._name = name;
-    this._cardSelected = cardSelected;
-    this._turn = false
+    this._pieceSelected = pieceSelected;
+    this._turn = false;
 }
 Player.prototype.setTurn = function(playerTurn) {
-    this._turn = playerTurn
+    this._turn = playerTurn;
+}
+
+var Board = function(game) {
+    this._game = game;
+    this._cell = new Array();
+}
+Board.prototype.addPiece = function(row, column, tatetiGame) {
+
+    }
+    //piece es un array
+
+var Cell = function(row, column) {
+    this._name = "celda";
+    this._row = row;
+    this._column = column;
+    this._busy = false;
 }
--- js/utils.js
+++ js/utils.js
@@ -1,53 +1,97 @@
-function selectCard(card) {
-    console.log("valor  " + card.value + "id  " + card.id);
-    var cardPlayer1 = document.getElementById("card_player1");
-    var cardPlayer2 = document.getElementById("card_player2");
+var prototyeTateti;
+var prototypeBoard;
 
-    if (card.id == cardPlayer1.id && card.selectedIndex == '1') {
-        cardPlayer2.selectedIndex = 2
-    } else if (card.id == cardPlayer1.id && card.selectedIndex == '2') {
-        cardPlayer2.selectedIndex = 1
-    } else if (card.id == cardPlayer2.id && card.selectedIndex == '1') {
-        cardPlayer1.selectedIndex = 2
-    } else if (card.id == cardPlayer2.id && card.selectedIndex == '2') {
-        cardPlayer1.selectedIndex = 1
+function selectPiece(piece) {
+    console.log("valor  " + piece.value + "id  " + piece.id);
+    var piecePlayer1 = document.getElementById("piece_player1");
+    var piecePlayer2 = document.getElementById("piece_player2");
+
+    if (piece.id == piecePlayer1.id && piece.selectedIndex == '1') {
+        piecePlayer2.selectedIndex = 2;
+    } else if (piece.id == piecePlayer1.id && piece.selectedIndex == '2') {
+        piecePlayer2.selectedIndex = 1;
+    } else if (piece.id == piecePlayer2.id && piece.selectedIndex == '1') {
+        piecePlayer1.selectedIndex = 2;
+    } else if (piece.id == piecePlayer2.id && piece.selectedIndex == '2') {
+        piecePlayer1.selectedIndex = 1;
     }
 }
 
 function startGame() {
     var namePlayer1 = document.forms["players"]['player1'].value;
     var namePlayer2 = document.forms["players"]['player2'].value;
-    var cardPlayer1 = document.forms["players"]["cardPlayer1"].value;
-    var cardPlayer2 = document.forms["players"]["cardPlayer2"].value;
+    var piecePlayer1 = document.forms["players"]["piecePlayer1"].value;
+    var piecePlayer2 = document.forms["players"]["piecePlayer2"].value;
 
     if (namePlayer1 == "" || namePlayer2 == "") {
         alert("Names must be filled out");
         return false;
     }
 
-    if (cardPlayer1 == "default" || cardPlayer2 == "default") {
-        alert("Cards must be selected");
+    if (piecePlayer1 == "default" || piecePlayer2 == "default") {
+        alert("pieces must be selected");
         return false;
     }
 
-    var prototyePlayer1 = new Player(namePlayer1, cardPlayer1);
-    var prototyePlayer2 = new Player(namePlayer2, cardPlayer2);
+    var prototyePlayer1 = new Player(namePlayer1, piecePlayer1);
+    var prototyePlayer2 = new Player(namePlayer2, piecePlayer2);
 
     //TODO determinar el turno aleatoriamente
-    prototyePlayer1.setTurn(true)
+    prototyePlayer1.setTurn(true);
 
-    var prototyeTateti = new TatetiGame([prototyePlayer1, prototyePlayer2], "Started");
+    prototyeTateti = new TatetiGame([prototyePlayer1, prototyePlayer2], "Started");
 
     console.log("Game started: " + prototyeTateti._player);
 
-    showTurn(prototyeTateti._player)
+    showTurn(prototyeTateti._player);
+    prepareBoard(prototyeTateti);
+
 }
 
 function showTurn(players) {
-    var displayLabel = document.getElementById('turnLabel')
+    var displayLabel = document.getElementById('turnLabel');
     players.forEach(element => {
         if (element._turn) {
-            displayLabel.innerText = 'Turno de ' + element._name
+            displayLabel.innerText = element._name + "'s turn. Piece: " + element._pieceSelected;
         }
     });
+}
+
+function prepareBoard(tatetiGame) {
+    var protorypeCell0 = new Cell(0, 0)
+    var protorypeCell1 = new Cell(0, 1)
+    var protorypeCell2 = new Cell(0, 2)
+    var protorypeCell3 = new Cell(1, 0)
+    var protorypeCell4 = new Cell(1, 1)
+    var protorypeCell5 = new Cell(1, 2)
+    var protorypeCell6 = new Cell(2, 0)
+    var protorypeCell7 = new Cell(2, 1)
+    var protorypeCell8 = new Cell(2, 2)
+
+    prototypeBoard = new Board(tatetiGame)
+    prototypeBoard._cell.push(protorypeCell0, protorypeCell1, protorypeCell2,
+        protorypeCell3, protorypeCell4, protorypeCell5,
+        protorypeCell6, protorypeCell7, protorypeCell8
+    )
+    console.log()
+}
+
+function play(btn, row, column) {
+
+    var displayPiece = document.getElementById(btn.id);
+
+    if (prototyeTateti != null) {
+        prototyeTateti._player.forEach(element => {
+            if (element._turn) {
+                displayPiece.value = element._pieceSelected;
+            }
+            element.setTurn(!element._turn)
+        });
+        showTurn(prototyeTateti._player)
+
+    } else {
+        alert("There is no players");
+        return false;
+    }
+    //prototypeBoard.addPiece(row, column, prototyeTateti)
 }
