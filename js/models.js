4f8ebcba194aff9be506b04c66a95e8711e16665
NahuelValencia <nahuel.valencia21@gmail.com>
2020-04-12 23:31:28 -0300

    check if cell is busy

--- js/models.js
+++ js/models.js
@@ -1,29 +1,39 @@
-var TatetiGame = function(player, state) {
+var TatetiGame = function(player, board) {
     this._player = player;
-    this._state = state;
+    this._board = board;
+    this.status = status;
+}
+TatetiGame.prototype.changeStatus = function(newStatus) {
+    this.status = newStatus;
 }
 
 var Player = function(name, pieceSelected) {
     this._name = name;
     this._pieceSelected = pieceSelected;
-    this._turn = false;
+    this.turn = false;
 }
 Player.prototype.setTurn = function(playerTurn) {
-    this._turn = playerTurn;
+    this.turn = playerTurn;
 }
 
 var Board = function(game) {
-    this._game = game;
-    this._cell = new Array();
+    //this._game = game;
+    this._cell = new Array()
 }
 Board.prototype.addPiece = function(row, column, tatetiGame) {
 
     }
     //piece es un array
 
-var Cell = function(row, column) {
-    this._name = "celda";
+var Cell = function(row, column, name) {
+    this._name = name;
     this._row = row;
     this._column = column;
-    this._busy = false;
+    this.busy = false;
+    this.isFree = function() {
+        return !this.busy
+    }
+}
+Cell.prototype.setCellStatus = function(busy) {
+    this.busy = busy;
 }
--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,6 @@
 var prototyeTateti;
-var prototypeBoard;
+var justStarted = true;
+var count = 0;
 
 function selectPiece(piece) {
     console.log("valor  " + piece.value + "id  " + piece.id);
@@ -17,7 +18,7 @@ function selectPiece(piece) {
     }
 }
 
-function startGame() {
+function prepareGame() {
     var namePlayer1 = document.forms["players"]['player1'].value;
     var namePlayer2 = document.forms["players"]['player2'].value;
     var piecePlayer1 = document.forms["players"]["piecePlayer1"].value;
@@ -33,59 +34,86 @@ function startGame() {
         return false;
     }
 
+    //create players
     var prototyePlayer1 = new Player(namePlayer1, piecePlayer1);
     var prototyePlayer2 = new Player(namePlayer2, piecePlayer2);
 
     //TODO determinar el turno aleatoriamente
     prototyePlayer1.setTurn(true);
 
-    prototyeTateti = new TatetiGame([prototyePlayer1, prototyePlayer2], "Started");
+    //create Board and Cells
+    var prototypeBoard = prepareBoard();
 
-    console.log("Game started: " + prototyeTateti._player);
+    //create the Game
+    prototyeTateti = new TatetiGame([prototyePlayer1, prototyePlayer2], prototypeBoard, "Started");
+
+    console.log("Game started: " + prototyeTateti.status);
 
     showTurn(prototyeTateti._player);
-    prepareBoard(prototyeTateti);
 
 }
 
 function showTurn(players) {
     var displayLabel = document.getElementById('turnLabel');
     players.forEach(element => {
-        if (element._turn) {
+        if (element.turn) {
             displayLabel.innerText = element._name + "'s turn. Piece: " + element._pieceSelected;
         }
     });
 }
 
-function prepareBoard(tatetiGame) {
-    var protorypeCell0 = new Cell(0, 0)
-    var protorypeCell1 = new Cell(0, 1)
-    var protorypeCell2 = new Cell(0, 2)
-    var protorypeCell3 = new Cell(1, 0)
-    var protorypeCell4 = new Cell(1, 1)
-    var protorypeCell5 = new Cell(1, 2)
-    var protorypeCell6 = new Cell(2, 0)
-    var protorypeCell7 = new Cell(2, 1)
-    var protorypeCell8 = new Cell(2, 2)
+function prepareBoard() {
+    var protorypeCell0 = new Cell(0, 0, "btn1");
+    var protorypeCell1 = new Cell(0, 1, "btn2");
+    var protorypeCell2 = new Cell(0, 2, "btn3");
+    var protorypeCell3 = new Cell(1, 0, "btn4");
+    var protorypeCell4 = new Cell(1, 1, "btn5");
+    var protorypeCell5 = new Cell(1, 2, "btn6");
+    var protorypeCell6 = new Cell(2, 0, "btn7");
+    var protorypeCell7 = new Cell(2, 1, "btn8");
+    var protorypeCell8 = new Cell(2, 2, "btn9");
 
-    prototypeBoard = new Board(tatetiGame)
-    prototypeBoard._cell.push(protorypeCell0, protorypeCell1, protorypeCell2,
-        protorypeCell3, protorypeCell4, protorypeCell5,
-        protorypeCell6, protorypeCell7, protorypeCell8
-    )
-    console.log()
+    var board = new Board();
+    board._cell.push([protorypeCell0, protorypeCell1, protorypeCell2], [protorypeCell3, protorypeCell4, protorypeCell5], [protorypeCell6, protorypeCell7, protorypeCell8]);
+
+    return board;
+
 }
 
-function play(btn, row, column) {
+function play(btn, row, column, name) {
+    if (justStarted) {
+        justStarted = false;
+        prototyeTateti.changeStatus("In progress");
+    }
+    console.log("Game in progress: " + prototyeTateti.status);
 
+    //check if the cell is busy otherwise, set cell Status as busy
+    var cellSelected = prototyeTateti._board._cell[row][column];
+    if (cellSelected._name == name && cellSelected.isFree()) {
+        count++;
+
+        cellSelected.setCellStatus(!cellSelected.busy);
+        console.log("Change status for cell " + cellSelected._name + " to " + cellSelected.busy);
+
+        drawPiece(btn);
+
+
+    } else {
+        alert("Cell already used");
+        return false;
+    }
+}
+
+function drawPiece(btn) {
     var displayPiece = document.getElementById(btn.id);
 
     if (prototyeTateti != null) {
+        //draw X or O and change turn
         prototyeTateti._player.forEach(element => {
-            if (element._turn) {
+            if (element.turn) {
                 displayPiece.value = element._pieceSelected;
             }
-            element.setTurn(!element._turn)
+            element.setTurn(!element.turn)
         });
         showTurn(prototyeTateti._player)
 
@@ -93,6 +121,7 @@ function play(btn, row, column) {
         alert("There is no players");
         return false;
     }
+
     //prototypeBoard.addPiece(row, column, prototyeTateti)
 }
 
